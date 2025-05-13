d40722944f3bdcc56cf832bd6116757aa67b0272
Baatyrbek <batrudinovshapiro@gmail.com>
2025-05-13 16:04:22 -0500

    broken window behavior

--- Module5/problem3.py
+++ Module5/problem3.py
@@ -2,12 +2,32 @@
 # 5/13/2025
 # What the program does 
 
+# !!!I get it now, terminal does not updating itself because of turtle screen. 
+# It creates a turtle screen but it's not showing above all other open windows. How do I fix it?
+
 import turtle
+
+while True:
+    try:
+        userSides = int(input("Hi, please enter your number of sides\n"))
+        userLength = int(input("Now, length of sides\n"))
+        
+        if userSides >=3 and userLength >0:
+            break
+        else: 
+            turtle.bye()
+            print("Please enter valid values")
+            exit()
+        
+    except (TypeError, ValueError):
+        turtle.bye()
+        print("Only numbers are valid")
+        exit()
+
 
 a = turtle.Screen()
 b = turtle.Turtle()
-userSides = input("Hi, please enter your number of sides")
-userLength = input("Now, length of sides")
+
 angle = 360.0 / userSides
 for i in range(userSides):
     turtle.forward(userLength)
