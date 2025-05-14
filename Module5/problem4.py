04c068b2ba2e7338ee291cb8abb226d88dcd3780
Baatyrbek <batrudinovshapiro@gmail.com>
2025-05-14 00:21:00 -0500

    problem 4

--- Module5/problem4.py
+++ Module5/problem4.py
@@ -1,3 +1,12 @@
 # Baatyrbek Turatov
 # 5/13/2025
 # What the program does
+
+nums = [i for i in range(1, 51)]
+for num in nums:
+    if num % 3 == 0 and num % 5 == 0:
+        print(f"{num} divisible by both 3 & 5")
+    elif num % 5 == 0:
+        print(f"{num} divisible by 5")
+    elif num % 3 == 0:
+        print(f"{num} divisible by 3")
